1b6115a6f75db4e5f2c24e197f82f6ecc8856554
wsb <767518216@qq.com>
2024-12-11 15:30:00 +0800

    feature:开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/wsb/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/wsb/shortlink/admin/service/UserService.java
@@ -3,6 +3,7 @@ package com.wsb.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wsb.shortlink.admin.dao.entity.UserDO;
+import com.wsb.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.wsb.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -12,9 +13,25 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 根据用户名查询用户信息
+     *
      * @param username 用户名
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户是否存在
+     *
+     * @param username 用户名
+     * @return 用户存在返回False，不存在返回True
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 
 }
--- admin/src/main/java/com/wsb/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/wsb/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.wsb.shortlink.admin.service.impl;
 
+import ch.qos.logback.core.net.server.Client;
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,16 +9,25 @@ import com.wsb.shortlink.admin.common.convention.exception.ClientException;
 import com.wsb.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.wsb.shortlink.admin.dao.entity.UserDO;
 import com.wsb.shortlink.admin.dao.mapper.UserMapper;
+import com.wsb.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.wsb.shortlink.admin.dto.resp.UserRespDTO;
 import com.wsb.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.wsb.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
+import static com.wsb.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
 /**
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -28,5 +39,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         }
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
