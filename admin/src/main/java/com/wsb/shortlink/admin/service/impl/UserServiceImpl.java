9091f7df7daa21be800b8fa8baf2facfb82e74ad
wsb <767518216@qq.com>
2024-12-18 15:00:06 +0800

    feature:开发短链接后管系统用户登录时间有效期修改为30天

--- admin/src/main/java/com/wsb/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/wsb/shortlink/admin/service/impl/UserServiceImpl.java
@@ -114,7 +114,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
          *  Value: Json字符串（用户信息）
          */
         stringRedisTemplate.opsForHash().put("login_" + requestParam.getUsername(), uuid, JSON.toJSONString(userDO));
-        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.MINUTES);
+        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.DAYS);
 
         return new UserLoginRespDTO(uuid);
     }
