0d0bd7124937d5c48a43186a0a27619b0288fc07
wsb <767518216@qq.com>
2025-02-27 19:25:53 +0800

    feature: 开发短链接监控之今日统计

--- project/src/main/java/com/wsb/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/wsb/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,15 +2,21 @@ package com.wsb.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.wsb.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 /**
  * 短链接今日统计实体
  */
-@TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_stats_today")
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -46,5 +52,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * 今日ip数
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
