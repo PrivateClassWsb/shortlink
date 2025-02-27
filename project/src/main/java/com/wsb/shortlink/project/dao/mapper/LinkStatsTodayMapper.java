0c4ea3777e6ca6d92d14c71db77b588e119b6043
wsb <767518216@qq.com>
2025-02-27 18:54:12 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/com/wsb/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.wsb.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wsb.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
