af31d454b2be22ea718a59f0297377f20c700d99
wsb <767518216@qq.com>
2025-03-03 19:35:50 +0800

    feature: 短链接创建或者修改时验证跳转链接白名单

--- project/src/main/java/com/wsb/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/wsb/shortlink/project/toolkit/LinkUtil.java
@@ -2,8 +2,10 @@ package com.wsb.shortlink.project.toolkit;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.Optional;
 
@@ -124,6 +126,29 @@ public class LinkUtil {
     public static String getNetwork(HttpServletRequest request) {
         String actualIp = getActualIp(request);
         return actualIp.startsWith("192.168.") || actualIp.startsWith("10.") ? "WIFI" : "Mobile";
+    }
+
+    /**
+     * 获取原始链接中的域名
+     * 如果原始链接包含 www 开头的话需要去掉
+     *
+     * @param url 创建或者修改短链接的原始链接
+     * @return 原始链接中的域名
+     */
+    public static String extractDomain(String url) {
+        String domain = null;
+        try {
+            URI uri = new URI(url);
+            String host = uri.getHost();
+            if (StrUtil.isNotBlank(host)) {
+                domain = host;
+                if (domain.startsWith("www.")) {
+                    domain = host.substring(4);
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        return domain;
     }
 
 }
