1772e0823e0ed385a73185140112b4594a1265e1
wsb <767518216@qq.com>
2025-03-04 19:13:40 +0800

    optimize: 优化短链接相关缓存 Key 规范

--- admin/src/main/java/com/wsb/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/wsb/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static com.wsb.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
 import static com.wsb.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 
@@ -53,7 +54,7 @@ public class UserTransmitFilter implements Filter {
                 }
                 Object userInfoJsonStr = null;
                 try{
-                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
                     if (userInfoJsonStr == null) {
                         throw new ClientException(USER_TOKEN_FAIL);
                     }
